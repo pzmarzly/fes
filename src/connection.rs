e0efc1c90db63be8fd4ce825e5bcc6a488a89f26
Paweł Zmarzły <pawo2500@gmail.com>
2019-04-27 23:53:31 +0200

    w

--- src/connection.rs
+++ src/connection.rs
@@ -1,47 +1,36 @@
-use futures::io::{AsyncReadExt, AsyncWriteExt};
-use protocol::Parcel;
-
 use crate::id::{Identity, PartialIdentity};
 use crate::proto::{ClientToServer::*, ServerToClient::*};
-use crate::util::ParcelExt;
-
-pub trait Stream: AsyncReadExt + AsyncWriteExt + Unpin {}
-impl<T: AsyncReadExt + AsyncWriteExt + Unpin> Stream for T {}
+use crate::util::{Stream, StreamWrapper};
 
 #[derive(Debug)]
-pub struct SecureConnection<T> {
+pub struct SecureConnection<T: Stream> {
     id: Identity,
-    stream: T,
+    stream: StreamWrapper<T>,
 }
 
 #[derive(Debug)]
-pub struct Connection<T> {
+pub struct Connection<T: Stream> {
     id: Identity,
-    stream: T,
+    stream: StreamWrapper<T>,
 }
 
 impl<T: Stream> Connection<T> {
     pub fn new(id: Identity, stream: T) -> Self {
-        Self { id, stream }
+        Self {
+            id,
+            stream: StreamWrapper::new(stream),
+        }
     }
 
     pub async fn client_side_upgrade(
         mut self,
         other: Option<PartialIdentity>,
     ) -> Result<SecureConnection<T>, futures::io::Error> {
-        await!(self.send(UpgradeRequest("fts 1".to_string())))?;
+        await!(self.stream.send(UpgradeRequest("fts 1".to_string())))?;
         Ok(SecureConnection {
             id: self.id,
             stream: self.stream,
         })
-    }
-
-    async fn send(&mut self, item: impl Parcel) -> Result<(), futures::io::Error> {
-        let data = item.to_bytes();
-        let data_len = data.len().to_le_bytes();
-        await!(self.stream.write_all(&data_len))?;
-        await!(self.stream.write_all(&data))?;
-        Ok(())
     }
 
     pub async fn server_side_upgrade(
--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,4 @@
+use futures::io::{AsyncReadExt, AsyncWriteExt};
 use protocol::{Error, Parcel, Settings};
 
 pub trait ParcelExt<T> {
@@ -12,5 +13,25 @@ impl<T: Parcel> ParcelExt<T> for T {
 
     fn from_bytes(bytes: &[u8]) -> Result<T, Error> {
         T::from_raw_bytes(bytes, &Settings::default())
+    }
+}
+
+pub trait Stream: AsyncReadExt + AsyncWriteExt + Unpin {}
+
+impl<T: AsyncReadExt + AsyncWriteExt + Unpin> Stream for T {}
+
+#[derive(Debug, PartialEq)]
+pub struct StreamWrapper<T: Stream>(pub T);
+
+impl<T: Stream> StreamWrapper<T> {
+    pub fn new(stream: T) -> Self {
+        Self(stream)
+    }
+    pub async fn send(&mut self, item: impl Parcel) -> Result<(), futures::io::Error> {
+        let data = item.to_bytes();
+        let data_len = data.len().to_le_bytes();
+        await!(self.0.write_all(&data_len))?;
+        await!(self.0.write_all(&data))?;
+        Ok(())
     }
 }
